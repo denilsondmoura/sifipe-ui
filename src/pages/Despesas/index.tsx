d56cebd77ddf23c117e392f6a8b6133cc0598652
denilsondmoura <denilsondmoura@cge.pi.gov.br>
2022-09-01 13:30:18 -0300

    Nada demais

--- src/pages/Despesas/index.tsx
+++ src/pages/Despesas/index.tsx
@@ -3,43 +3,60 @@ import { Col, Container, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
-import { Section } from "./style";
+import { WhiteBoard, BackgroundTop, BackgroundBot, Content } from "./style";
 
 export const DespesasView = function () {
 
     return (
+        <>
+            <BackgroundTop>.</BackgroundTop>
+            <BackgroundBot>.</BackgroundBot>
+            <WhiteBoard>
+                <Content>.</Content>
+                {/* <Form>
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </Form> */}
+            </WhiteBoard>
+        </>
         // <Container>
-            <Container className="justify-content" >
-                <Row>
-                    <Col md={8}>
+            // <Container className="justify-content" >
+            //     <Row>
+            //         <Col md={8}>
                           
-                    <Row>
-                        <Col md={9}>
-                            <Form.Group className="mb-3" controlId="formTipoDespessa">
-                                <Form.Label>Tipo</Form.Label>
-                                <Form.Select aria-label="Default select example">
-                                    <option>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </Form.Select>
-                            </Form.Group>
-                        </Col>
-                        <Col md={3}>
-                            <Form.Group className="mb-3" controlId="formValorDespesa">
-                                <Form.Label>Valor (R$)</Form.Label>
-                                <Form.Control type="text" placeholder="Ex.: 20,00" />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Button style={{backgroundColor: "#115571"}} variant="primary" type="submit">
-                            Registrar
-                        </Button>
-                    </Row>
-                    </Col>
-                </Row>                
-            </Container>
+            //         <Row>
+            //             <Col md={9}>
+            //                 <Form.Group className="mb-3" controlId="formTipoDespessa">
+            //                     <Form.Label>Tipo</Form.Label>
+            //                     <Form.Select aria-label="Default select example">
+            //                         <option>Open this select menu</option>
+            //                         <option value="1">One</option>
+            //                         <option value="2">Two</option>
+            //                         <option value="3">Three</option>
+            //                     </Form.Select>
+            //                 </Form.Group>
+            //             </Col>
+            //             <Col md={3}>
+            //                 <Form.Group className="mb-3" controlId="formValorDespesa">
+            //                     <Form.Label>Valor (R$)</Form.Label>
+            //                     <Form.Control type="text" placeholder="Ex.: 20,00" />
+            //                 </Form.Group>
+            //             </Col>
+            //         </Row>
+            //         <Row>
+            //             <Button style={{backgroundColor: "#115571"}} variant="primary" type="submit">
+            //                 Registrar
+            //             </Button>
+            //         </Row>
+            //         </Col>
+            //     </Row>                
+            // </Container>
         // </Container>
     )
 }
--- src/pages/Despesas/style.ts
+++ src/pages/Despesas/style.ts
@@ -3,14 +3,31 @@ import styled from "styled-components";
 // primary #31AFB4
 // secundary #115571
 
-export const Section = styled.section`
-    background-color: #f5f5f5;
-    color: #fff;
-    justify-content: center;
-    width: 70%;
-    margin: 20px auto;
-    padding: 20px;
-    border: solid 1px #115571;
-    border-radius: 8px;
-    box-shadow: 1px 1px #115571;
+export const BackgroundTop = styled.div`
+    background-color: #31AFB4;
+    width: 100%;
+    height: 50%;
 `
+export const BackgroundBot = styled.div`
+    background-color: #115571;
+    width: 100%;
+    height: 50%;
+`
+
+export const WhiteBoard = styled.div`
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    width: 60%;
+    height: 80%;
+    margin: auto;
+    position: absolute;
+    top: 10%;
+    left: 20%;
+`
+
+export const Content = styled.div`
+    background-color: red;
+    margin: auto;
+    height: 30px;
+`
+
